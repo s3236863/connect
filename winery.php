f71678e640631b514ddda63b6a2dc3366024ec2f
Nathan <s3236863@student.rmit.edu.au>
2012-07-21 15:39:13 +1000

    added

--- winery.php
+++ winery.php
@@ -8,24 +8,27 @@
 	
 	<h1>Search wines and wineries</h1>
 	
-	<form>
-	Wine Name: <input type="text" name="winename" 
-id="winename" />
-	Winery Name: <input type="text" name="wineryname" 
-id="wineryname" />
-	Region: 
+	<form action="winery.php" method="post" onsubmit="return checkWine()" >
+	<table id="winetable">
+	<tr><td>Wine Name: </td><td><input type="text" name="winename" 
+id="winename" /></td></tr>
+	<tr><td>Winery Name: </td><td><input type="text" name="wineryname" 
+id="wineryname" /></td></tr>
+	<tr><td>Region: </td><td>
 	<select>
 	<!--dynamically added select options-->
 		<option></option>
 	</select>
+	</td></tr>
 	
-	Grape variety:
+	<tr><td>Grape variety:</td><td>
 	<select>
 	<!--dynamically added select options-->
 		<option></option>
 	</select>
+	</td></tr>
 	
-	Year Range:
+	<tr><td>Year Range:</td><td>
 	<select>
 	<!--dynamically added select options-->
 		<option></option>
@@ -35,34 +38,37 @@ id="wineryname" />
 	 <!--dynamically added select options-->
 		<option></option>
 	 </select>
+	</td></tr>
 	 
-	 Minimum number of wines in stock, per wine: <input 
-type="text" name="minstock" id="minstock" />
-	 Minimum number of wines ordered, per wine: <input 
-type="text" name="minordered" id="minordered" />
-	 Dollar Cost Range:
+	<tr><td> Minimum number of wines in stock, per wine: </td><td><input 
+type="text" name="minstock" id="minstock" /></td></tr>
+	<tr><td> Minimum number of wines ordered, per wine: </td><td><input 
+type="text" name="minordered" id="minordered" /></td></tr>
+	<tr><td> Dollar Cost Range:</td></tr>
 	 Minimum: <input type="text" name="minDollar" 
 id="minDollar" />
 	 Maximum: <input type="text" name="maxDollar" 
 id="maxDollar" />
+	</td></tr>
 	 
-	 <input type="submit" value="Search" name="search" 
+	</table>
+	 <br /><input type="submit" value="Search" name="search" 
 id="search" />
 	
 	</form>
 	
 	<?php
 	
-	$winename = POST['winename'];
-	$wineryname = POST['wineryname'];
-	$region = POST['region'];
-	$grape = POST['grape'];
-	$YearMin = POST['yearMin'];
-	$YearMax = POST['yearMax'];
-	$minstock = POST['minstock'];
-	$minordered = POST['minordered'];
-	$minDollar = POST['minDollar'];
-	$maxDollar = POST['maxDollar'];
+	$winename = $_POST['winename'];
+	$wineryname = $_POST['wineryname'];
+	$region = $_POST['region'];
+	$grape = $_POST['grape'];
+	$YearMin = $_POST['yearMin'];
+	$YearMax = $_POST['yearMax'];
+	$minstock = $_POST['minstock'];
+	$minordered = $_POST['minordered'];
+	$minDollar = $_POST['minDollar'];
+	$maxDollar = $_POST['maxDollar'];
 	$counter = 0;
 	
 	//enter the database and query it
