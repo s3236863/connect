eef74de1bc9e64803d1d9550418cc134debb2dd4
Nathan Dalby <s3236863@student.rmit.edu.au>
2012-07-31 13:08:03 +1000

    added

--- winery.php
+++ winery.php
@@ -160,7 +160,11 @@ $minordered != '' && $minDollar != '' && $maxDollar != '')
 
 //this is the (very long) sql command as a variable name
 //still needed to be added to it will be querying each of the variables against it.
-	$query = "select wine_name, variety, year,  winery_name, region_name, cost, on_hand, qty, sum(cost*qty) as Total_Revenue from wine, grape_variety, winery, region, inventory, wine_variety, items where winery.region_id = region.region_id and wine.winery_id = winery.winery_id and wine.wine_id = inventory.wine_id and wine_variety.variety_id = grape_variety.variety_id and wine_variety.wine_id = wine.wine_id and items.wine_id = wine.wine_id group by wine.wine_id";
+	$query = "select wine_name, variety, year,  winery_name, region_name, cost, on_hand, qty, sum(cost*qty) as Total_Revenue from wine, grape_variety, winery, region, inventory, wine_variety, items where winery.region_id = 
+region.region_id and wine.winery_id = winery.winery_id and wine.wine_id = inventory.wine_id and wine_variety.variety_id = grape_variety.variety_id and wine_variety.wine_id = wine.wine_id and items.wine_id = wine.wine_id group 
+by wine.wine_id and wine_name like '%$winename%' and variety like '%$grape%' and year between '$YearMin' 
+and '$YearMax' and winery_name like '%$wineryname%' and region_name like '%$region%' and cost between 
+'$minDollar' and '$maxDollar' and qty between '$minStock' and '$minordered'";
 
 	print "</span>";
 
